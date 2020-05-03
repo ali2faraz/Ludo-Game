a9d1d57488624a0ffab823e297630fc4cd0db53e
Lodhicoder <bluewali786@gmail.com>
2020-05-03 20:37:41 +0500

    lol

--- Ludo/Ludo/Box.cpp
+++ Ludo/Ludo/Box.cpp
@@ -1,1 +1,36 @@
-#include"Box.h"
+#include "Box.h"
+#include<iostream>
+using namespace std;
+
+Box::Box()
+{
+
+}
+Box::~Box()
+{
+
+}
+void Box::AddPiece()
+{
+
+}
+void Box::RemovePiece()
+{
+
+}
+void Box::SetColour()
+{
+
+}
+void Box::SetGoal()
+{
+
+}
+void Box::SetHouse()
+{
+
+}
+void Box::SetNumber()
+{
+
+}
--- Ludo/Ludo/View.cpp
+++ Ludo/Ludo/View.cpp
@@ -1,1 +1,35 @@
-#include"View.h"
+#include "View.h"
+#include<iostream>
+using namespace std;
+View::View()
+{
+
+}
+View::~View()
+{
+
+}
+void View::StartGame()
+{
+
+}
+void View::EndGame()
+{
+
+}
+void View::InitBoard()
+{
+
+}
+void View::RollDice()
+{
+
+}
+void View::DisableDice()
+{
+
+}
+void View::UpdateBoard()
+{
+
+}
